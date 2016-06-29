665ed975e8ac09410824e95e2452a861397b6d0c
徐峰 <Peak_Tsui@outlook.com>
2016-06-29 18:04:43 +0800

    demo

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        //static RedisClient redisClient = new RedisClient("127.0.0.1", 6379);//redis服务IP和端口 6379默认端口
+        static void Main(string[] args)
+        {
+            //字符串
+            //RedisDemo.StringTest();
+
+            //Hash
+            //RedisDemo.HashTest();
+
+            //List
+            RedisDemo.ListTest();
+
+            //Set
+            //RedisDemo.SetTest();
+
+            //SortedSet
+            //RedisDemo.SortedSet();
+        }
+
+
+    }
+}
